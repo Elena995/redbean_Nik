0b8fc84ee7a5802667e1bfcf1baf4335ac1a5782
Elena995 <41616043+Elena995@users.noreply.github.com>
2018-07-24 11:49:37 +0300

    Initial commit

---
+++ views/v_pagination.php
@@ -0,0 +1,28 @@
+<?php
+echo'
+<div class="row">
+    <div class="col-md-12">
+        <nav class="stranizy">
+            <div class="straniza">
+                <ul class=" pagination">
+                    <li>
+                        <a href="" aria-label="Previous"><span aria-hidden="true">&laquo</span></a>
+                    </li>
+                    <li><a href="">1</a></li>
+                    <li><a href="">2</a></li>
+                    <li><a href="">3</a></li>
+                    <li><a href="">4</a></li>
+                    <li><a href="">5</a></li>
+                    <li><a href="">6</a></li>
+                    <li><a href="">7</a></li>
+                    <li><a href="">8</a></li>
+                    <li>
+                        <a href="" aria-label="Previous"><span aria-hidden="true">&raquo</span></a>
+                    </li>
+
+                </ul>
+            </div>
+        </nav>
+    </div>     
+</div>
+';
---
+++ views/v_search_result.php
@@ -0,0 +1,39 @@
+
+<?php
+echo'
+<div class=\"row\">  <!------------------------------------------CONTENT-------------------------------------------------------------->
+	<div class=\"col-md-12\">
+		<div class=\"content\">
+			<div class=\"anketas\">
+';
+for ($i=0;$i<7;$i++){
+	echo'
+				<div class="list">
+					<div class="figure " >
+						<div class="pris"><i class="fa fa-rub" aria-hidden="true"></i>'.
+						   $anketas[$i]['price_1h_challenge'].' </div>
+						<figure><a href="#"><img src="../img/3.jpg" alt="salon"></a></figure>
+
+					</div>
+
+					<div class="name-panel " >
+						<a href="#" alt="name"><h5>SPA NEW</h5></a>
+
+					</div>
+					<div class="geo">
+
+						<b> Москва</b>:
+						<span class="metro_list">Маяковская </span>,
+						<span class="metro_list">Тверская</span>,
+						<span class="metro_list">Белорусская </span>.
+					</div>
+				</div> 
+				';
+}
+echo'
+			</div>
+		</div>
+	</div>
+</div>
+';
+
